0e2c1de1303d8c999a6085ba84f20a906302bfc1
pratik-kshetree <kshetreepratik@gmail.com>
2024-09-09 16:18:34 +0000

    object

--- .devcontainer/basics-2/object.js
+++ .devcontainer/basics-2/object.js
@@ -1,11 +1,39 @@
-let sym=Symbol("key")
-let myObj={
-    name:"pratik",
-    age:19,
-    location:"btwl",
-    [sym]:"key", // at first we have to declare symbol to put synmol in object
-}
-console.log(myObj.age);
-console.log(myObj);
-console.log(myObj[sym]);
+// let sym=Symbol("key")
+// let myObj={
+//     name:"pratik",
+//     age:19,
+//     location:"btwl",
+//     [sym]:"key", // at first we have to declare symbol to put synmol in object
+// }
+// console.log(myObj.age);
+// console.log(myObj);
+// console.log(myObj[sym]);
+
+//----------- SECOND PART
+
+// let bio={
+//     adreess:"bhw",
+//     mail:"qwerty",
+//     fullname:{
+//         fName:"pratik",
+//         lname:"kshetree"
+//     },
+//     age:19
+// }
+// console.log(bio);
+// console.log(bio.fullname.fName);
+// console.log(bio.fullname);
+
+let obj1={1:"a",2:"b"}
+let obj2={1:"a",2:"b"}
+let obj3={1:"a",2:"b"}
+
+let obj4=Object.assign({},obj1,obj2,obj3)
+console.log(obj4);
+
+
+
+//paart 17  10min
+
+
 
