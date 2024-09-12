2bce83c9569aeb4d310b243e05212287c96c92ec
pratik-kshetree <kshetreepratik@gmail.com>
2024-09-12 15:57:35 +0000

    ifelse

---
+++ .devcontainer/control-flow/first.js
@@ -0,0 +1,28 @@
+let username="pratik"
+let pw=true
+let email=true
+let logedinfb=false
+
+//both the condition should be true torun in and (&&) case
+if( username && pw){
+    console.log("process successfull");
+    
+} 
+if( username && logedinfb){
+    console.log(" process successfull");
+    
+} 
+else{
+    console.log("unsuccessfull");
+    
+}
+// in or (||) case if only one condition is true then it will run
+if( username || logedinfb){
+    console.log(" process successfull");
+    
+} 
+else{
+    console.log("unsuccessfull");
+    
+}
+//26:36
