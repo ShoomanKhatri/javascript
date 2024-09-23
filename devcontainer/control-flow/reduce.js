13afab7bfa18c9064786fd373a5d70ec18ca73df
pratik-kshetree <kshetreepratik@gmail.com>
2024-09-23 13:44:24 +0000

    reduce

---
+++ devcontainer/control-flow/reduce.js
@@ -0,0 +1,7 @@
+let mynum =[1,2,3,4]
+let initialvalue = 0
+
+let store= mynum.reduce((accumulator,currentvalue) => accumulator + currentvalue,initialvalue)
+//console.log("acc :",accumulator ,"curvalu:", currentvalue);
+
+console.log(store);
