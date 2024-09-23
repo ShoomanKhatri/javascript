a8a1e280915a89a21c2813cbf85e136b865984b3
pratik-kshetree <kshetreepratik@gmail.com>
2024-09-23 13:30:57 +0000

    date&time

---
+++ devcontainer/basics/datetime.js
@@ -0,0 +1,33 @@
+// let myDate= new Date()
+// console.log(myDate)
+// console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+
+
+// let nextdate=new Date(2005 ,1,25 )
+// console.log(nextdate);
+// console.log(nextdate.toString())
+
+// let nextdate=new Date(2005 ,1,25,11,50 )
+// console.log(nextdate);
+// console.log(nextdate.toLocaleString())
+
+// let nextdate=new Date("2005 ,2,25" )
+// console.log(nextdate);
+// console.log(nextdate.toLocaleString())
+
+let date=new Date()
+console.log(new Date().getFullYear());
+console.log(`The year is ${new Date().getFullYear()}`);
+console.log("The year is",date.getFullYear(),", the month is",date.getMonth(),"and the day is",
+date.getDay()+1);
+
+
+
+
+
+
