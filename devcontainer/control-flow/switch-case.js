eefc83e6928691856bb74b45e2fcf5cfe676eb4f
pratik-kshetree <kshetreepratik@gmail.com>
2024-09-23 13:45:26 +0000

    switch-case

---
+++ devcontainer/control-flow/switch-case.js
@@ -0,0 +1,15 @@
+let mynum=4
+switch (mynum) {
+    case 1:console.log("correct1");
+      break;
+    case 2:console.log("correct2");
+      break;
+    case 3:console.log("correct3");
+      break;
+    case 4:console.log("correct4");
+      break;
+
+    default:console.log("notcorrect");
+    
+        break;
+}
