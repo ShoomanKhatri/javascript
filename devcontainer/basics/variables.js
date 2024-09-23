e31a43bdfd75252d29592748233ed7aa57ffe64a
pratik-kshetree <kshetreepratik@gmail.com>
2024-09-23 13:32:03 +0000

    variable

---
+++ devcontainer/basics/variables.js
@@ -0,0 +1,18 @@
+// let scpe="bca"
+// let margin="4"
+// const year=2
+// console.log(scpe);
+// console.log(year);
+// console.table([scpe,margin,year]);
+let score=33
+console.log(typeof score);
+let change=String(score);//in js semicolon is not mandatory
+console.log( typeof change)
+console.log(5>2);
+console.log("2"==2);
+console.log("2"===2);
+
+
+
+
+
